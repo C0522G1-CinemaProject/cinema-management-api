d7e8f5b39646993a31e006757e0f7533bb3236ce
minhtri1709 <trihoangminh52@gmail.com>
2022-11-10 15:52:19 +0700

    fix delete TriHM

--- project-backend/src/test/java/projectbackend/controller/MovieRestControllerTest/MovieRestController_getList.java
+++ project-backend/src/test/java/projectbackend/controller/MovieRestControllerTest/MovieRestController_getList.java
@@ -85,36 +85,5 @@ public class MovieRestController_getList {
                 .andExpect(status().isOk());
     }
 
-    //test id = null
-    @Test
-    public void delete_1() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders.delete("/api/movie/delete/null"))
-                .andDo(print())
-                .andExpect(status().isBadRequest());
-    }
 
-    //test id = "" -> Not Found
-    @Test
-    public void delete_2() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders.delete("/api/movie/delete/"))
-                .andDo(print())
-                .andExpect(status().isBadRequest());
-    }
-
-    //test id = 11 --> Not Found
-    @Test
-    public void delete_3() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders.delete("/api/movie/delete/11/"))
-                .andDo(print())
-                .andExpect(status().isNoContent());
-    }
-
-    //test id = 1 -->Ok
-
-    @Test
-    public void delete_4() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders.delete("/api/movie/delete/1/"))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
 }
