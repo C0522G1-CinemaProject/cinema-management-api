80685dacb642e21c2bcd412612bc6f8035db6dad
namhuynn1911 <namhuynh191193@gmail.com>
2022-11-10 22:45:12 +0700

    update query

--- project-backend/src/main/java/projectbackend/repository/movie/IMovieRepository.java
+++ project-backend/src/main/java/projectbackend/repository/movie/IMovieRepository.java
@@ -14,18 +14,24 @@ public interface IMovieRepository extends JpaRepository<Movie, Integer> {
     @Query(value = "select  movie.name as movieName,movie.image as movieImage,show_times.date_projection as showTimeDate" +
             ",movie.film_time as movieFilmTime from movie " +
             "join show_times on movie.id = show_times.movie_id " +
-            "where ((day(show_times.date_projection)+3) >= day(curdate())) " +
-            "and  (month(show_times.date_projection) >= month(curdate())) " +
+            "where ((day(show_times.date_projection)+3) >= day(curdate()) " +
+            "    and  month(show_times.date_projection) >= month(curdate())) " +
+            "or" +
+            "((day(show_times.date_projection)+3) < day(curdate()) " +
+            "    and  month(show_times.date_projection) > month(curdate()))" +
             "AND (year(show_times.date_projection) = year(curdate())) " +
             "and name like %:keywordName% " +
             "and movie.is_delete=0",
             countQuery = "select count(*) " +
                     "from movie " +
                     "join show_times on movie.id = show_times.movie_id " +
-                    "where ((DAY(show_times.date_projection)+3) >= day(curdate())) " +
-                    "and  (month(show_times.date_projection) >= month(curdate())) " +
+                    "where ((day(show_times.date_projection)+3) >= day(curdate()) " +
+                    "    and  month(show_times.date_projection) >= month(curdate())) " +
+                    "or" +
+                    "((day(show_times.date_projection)+3) < day(curdate()) " +
+                    "    and  month(show_times.date_projection) > month(curdate()))" +
                     "AND (year(show_times.date_projection) = year(curdate())) " +
                     "and name like %:keywordName% " +
-                    "and movie.is_delete=0",nativeQuery = true)
+                    "and movie.is_delete=0", nativeQuery = true)
     Page<IMovieDto> findAllMovie(@Param("keywordName") String name, Pageable pageable);
 }
