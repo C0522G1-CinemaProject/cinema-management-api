78582efb559c79382e07caee1ff6fc5fcdb5b566
daiphanphuoc <daiphanphuoc@gmail.com>
2022-11-10 09:23:48 +0700

    second day coding

--- project-backend/src/main/java/projectbackend/model/decentralization/User.java
+++ project-backend/src/main/java/projectbackend/model/decentralization/User.java
@@ -1,31 +1,44 @@
 package projectbackend.model.decentralization;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class User {
 
     @Id
-    private String userName;
+    private String username;
     private String password;
     private boolean isDelete;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "username"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
 
     public User() {
     }
 
-    public User(String userName, String password, boolean isDelete) {
-        this.userName = userName;
+    public User(String username, String password, boolean isDelete, List<Role> roles) {
+        this.username = username;
         this.password = password;
         this.isDelete = isDelete;
+        this.roles = roles;
     }
 
-    public String getUserName() {
-        return userName;
+    public List<Role> getRoles() {
+        return roles;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String userName) {
+        this.username = userName;
     }
 
     public String getPassword() {
