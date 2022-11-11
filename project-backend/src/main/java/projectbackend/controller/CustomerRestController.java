3ec3d1a32a377dbcf8f2a63eda281c6d7fb7451f
unknown <vovanty0504@gmail.comgit config --global user.name vovanty0504>
2022-11-11 10:32:34 +0700

    TyVV-customer

--- project-backend/src/main/java/projectbackend/controller/CustomerRestController.java
+++ project-backend/src/main/java/projectbackend/controller/CustomerRestController.java
@@ -1,94 +1,22 @@
 package projectbackend.controller;
 
-<<<<<<< HEAD
-=======
 import org.springframework.beans.BeanUtils;
->>>>>>> a349e91ab233bd662eb3b822f5229e701bf146eb
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.*;
-import projectbackend.dto.ticket.ITicketDto;
-import projectbackend.model.ticket.Ticket;
-import projectbackend.service.ticket.ITicketService;
-
-import java.util.Optional;
-=======
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import projectbackend.dto.customer.CustomerDto;
 import projectbackend.model.customer.Customer;
 import projectbackend.service.customer.ICustomerService;
 import projectbackend.service.customer.ICustomerTypeService;
->>>>>>> a349e91ab233bd662eb3b822f5229e701bf146eb
 
 import javax.validation.Valid;
 
-@CrossOrigin
 @RestController
-<<<<<<< HEAD
-@CrossOrigin("*")
-@RequestMapping("api")
-public class CustomerRestController {
-    @Autowired
-    ITicketService iTicketService;
-
-    @GetMapping("/booking/ticket")
-    public ResponseEntity<Page<ITicketDto>> showListBookingTicket(@PageableDefault(value = 5) Pageable pageable) {
-        Page<ITicketDto> ticketPage = iTicketService.findAllBookingTickets(pageable, "abristog");
-        if (ticketPage.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(ticketPage, HttpStatus.OK);
-    }
-
-    @GetMapping("/canceled/ticket")
-    public ResponseEntity<Page<ITicketDto>> showListCanceledTicket(@PageableDefault(value = 5) Pageable pageable) {
-        Page<ITicketDto> ticketPage = iTicketService.findAllCanceledTickets(pageable, "admin");
-        if (ticketPage.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(ticketPage, HttpStatus.OK);
-    }
-
-
-    @GetMapping("/history/point")
-    public ResponseEntity<Page<ITicketDto>> showListHistoryPoint(
-            @PageableDefault(value = 5) Pageable pageable,
-            @RequestParam(value = "bookingTime", defaultValue = "", required = false) String bookingTime,
-            @RequestParam(value = "price", defaultValue = "0") int value) {
-
-        Page<ITicketDto> historyPointSearch;
-        if (value == 0) {
-            historyPointSearch = iTicketService.findAllHistoryPoint("abristog", bookingTime, pageable);
-        } else {
-            historyPointSearch = iTicketService.findAllHistoryPointSearch("abristog", bookingTime, value, pageable);
-        }
-
-        if (historyPointSearch.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(historyPointSearch, HttpStatus.OK);
-
-    }
-
-
-    @DeleteMapping("delete/ticket/{id}")
-    public ResponseEntity<Ticket> deleteTicket(@PathVariable Integer id) {
-        Optional<Ticket> ticket = iTicketService.findByIdTicKet(id);
-        System.out.println(iTicketService.findByIdTicKet(id).get());
-        if (ticket.isPresent()) {
-            iTicketService.deleteTicket(id);
-            return new ResponseEntity<>(HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-}
-=======
 @RequestMapping("/api/customer")
 public class CustomerRestController {
 
@@ -126,10 +54,10 @@ public class CustomerRestController {
             return new ResponseEntity<>(HttpStatus.OK);
         }
     }
+
     @GetMapping("/find/{id}")
     public ResponseEntity<Customer> editCustomer(@PathVariable Integer id) {
         Customer customer = iCustomerService.findByIdCustomer(id).get();
         return new ResponseEntity<>(customer, HttpStatus.OK);
     }
 }
->>>>>>> a349e91ab233bd662eb3b822f5229e701bf146eb
--- project-backend/src/main/java/projectbackend/controller/TicketRestController.java
+++ project-backend/src/main/java/projectbackend/controller/TicketRestController.java
@@ -14,8 +14,61 @@ import projectbackend.service.ticket.ITicketService;
 
 import java.util.Optional;
 
-
+@CrossOrigin("*")
+@RequestMapping("api")
 @RestController
 public class TicketRestController {
+    @Autowired
+    ITicketService iTicketService;
 
+    @GetMapping("/booking/ticket")
+    public ResponseEntity<Page<ITicketDto>> showListBookingTicket(@PageableDefault(value = 5) Pageable pageable) {
+        Page<ITicketDto> ticketPage = iTicketService.findAllBookingTickets(pageable, "abristog");
+        if (ticketPage.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(ticketPage, HttpStatus.OK);
+    }
+
+    @GetMapping("/canceled/ticket")
+    public ResponseEntity<Page<ITicketDto>> showListCanceledTicket(@PageableDefault(value = 5) Pageable pageable) {
+        Page<ITicketDto> ticketPage = iTicketService.findAllCanceledTickets(pageable, "admin");
+        if (ticketPage.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(ticketPage, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/history/point")
+    public ResponseEntity<Page<ITicketDto>> showListHistoryPoint(
+            @PageableDefault(value = 5) Pageable pageable,
+            @RequestParam(value = "bookingTime", defaultValue = "", required = false) String bookingTime,
+            @RequestParam(value = "price", defaultValue = "0") int value) {
+
+        Page<ITicketDto> historyPointSearch;
+        if (value == 0) {
+            historyPointSearch = iTicketService.findAllHistoryPoint("abristog", bookingTime, pageable);
+        } else {
+            historyPointSearch = iTicketService.findAllHistoryPointSearch("abristog", bookingTime, value, pageable);
+        }
+
+        if (historyPointSearch.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(historyPointSearch, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("delete/ticket/{id}")
+    public ResponseEntity<Ticket> deleteTicket(@PathVariable Integer id) {
+        Optional<Ticket> ticket = iTicketService.findByIdTicKet(id);
+        System.out.println(iTicketService.findByIdTicKet(id).get());
+        if (ticket.isPresent()) {
+            iTicketService.deleteTicket(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
+
